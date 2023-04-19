19e7d2feb3aa70977323249c04625dbe6cc3aa7c
Lemon <lisandro.m.yo@gmail.com>
2023-04-19 20:36:51 -0300

    pdf

--- assets/carrito.js
+++ assets/carrito.js
@@ -110,3 +110,32 @@ payout.onclick = ()=>{
     icon: "success",
   });
 }
+
+/* ------------------------- Exportar a PDF -------------------------------------------- */
+let modal = document.getElementById("modalBuy");
+let pdf = document.getElementById("toPDF");
+
+pdf.onclick = function() {
+  var doc = new jsPDF('p', 'pt',
+  'letter');
+  var margin = 20;
+  var scale = (doc.internal.pageSize.width - margin * 2) / document.body.clientWidth;
+  var scale_mobile = (doc. internal.pageSize.width - margin * 2) /
+  document.body.getBoundingClientRect ();
+  // checking
+  // checking
+  if(/Android| webOS |iPhone| iPad|iPod|
+  BlackBerry| TEMobile|Opera Mini/i.test
+  (navigator.userAgent)) {
+  // mobile
+  doc.html(modal, {
+  x: margin,
+  y: margin,
+  html2canvas: {
+  scale: scale_mobile,
+  },
+  callback: function (doc)
+  doc.output('dataurlneewwindow'
+  {filename:'pdf.pdf'})
+
+}}}
