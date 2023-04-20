600cd35b55c00db7f3a5576378a4a4c73f0ee6ab
Lemon <lisandro.m.yo@gmail.com>
2023-04-20 09:47:56 -0300

    jquery

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+$(function(){
+    $("#theme").click(function(){
+        $("main").css({
+
+            
+            "background-image":"url(../images/pcquienes.jpg)",
+            // "background-color":"white",
+            "color":"#000",
+        });
+        $("h1, h2, p ,b").css({
+
+            
+       
+            "color":"#000",
+        });
+    })
+   
+     
+    
+});
